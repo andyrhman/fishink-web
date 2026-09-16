4f447504ea72fcae9ab1d7a9e487d6c3946f0b36
Andy <andyrhmann@gmail.com>
2026-09-16 22:45:19 +0800

    Added new cloudfront api with https

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -22,7 +22,7 @@ import enLocale from "i18n-iso-countries/langs/en.json";
 
 countries.registerLocale(enLocale);
 
-const API_BASE = "http://fishink-env.eba-wsxhuzjp.ap-southeast-3.elasticbeanstalk.com/api";
+const API_BASE = "https://d1wnqwlidsycvb.cloudfront.net/api";
 
 async function postJson(endpoint, url) {
 
